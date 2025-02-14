1995c133f14b5032f59f7102f81f208b180fb729
yanabatsuk <yana.batsuk0@gmail.com>
2025-02-14 15:06:25 -0500

    feat(savings): add Savings class inheriting from BankAccount - Implemented Savings class with inheritance from BankAccount - Initialized savings_amount attribute to track additional savings deposits - Ensured proper initialization with customer_name, customer_balance, and minimum_balance

--- savings.py
+++ savings.py
@@ -1,1 +1,9 @@
-print("Testing")
+from main import BankAccount
+
+class Savings(BankAccount):
+    def __init__(self, customer_name, customer_balance, minimum_balance, savings_amount):
+        # initializing parent class - BankAccount
+        super().__init__(customer_name, customer_balance, minimum_balance)
+        # additional savings deposit
+        self.savings_amount = savings_amount
+        savings_amount = 0
