639b81a7aefd3a22ccf411960a5600ffd379702a
yanabatsuk <yana.batsuk0@gmail.com>
2025-02-14 15:11:38 -0500

    feat(savings): added add_savings method to Savings class - Implemeneted add_savings method to allow depositing additional savings - Updates both savings_amount and customer_balance upon deposit

--- savings.py
+++ savings.py
@@ -7,3 +7,10 @@ class Savings(BankAccount):
         # additional savings deposit
         self.savings_amount = savings_amount
         savings_amount = 0
+
+    # method to add savings deposit
+    def add_savings(self, amount):
+        self.savings_amount += amount
+        self.customer_balance += amount
+        print(
+            f"${amount} added to savings. New savings balance: {self.savings_amount}. Total balance: ${self.customer_balance}.")
