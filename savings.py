291d1cd6d9d63fcde6b4b6a55cd434ca05723cfc
yanabatsuk <yana.batsuk0@gmail.com>
2025-02-14 15:14:07 -0500

    feat(savings): add print_account_informaiton method - Displays savings balance along with customer information

--- savings.py
+++ savings.py
@@ -12,5 +12,9 @@ class Savings(BankAccount):
     def add_savings(self, amount):
         self.savings_amount += amount
         self.customer_balance += amount
-        print(
-            f"${amount} added to savings. New savings balance: {self.savings_amount}. Total balance: ${self.customer_balance}.")
+        print(f"${amount} added to savings. New savings balance: {self.savings_amount}. Total balance: ${self.customer_balance}.")
+
+    # method to print savings account information
+    def print_account_information(self):
+        self.print_customer_information()  # calls the inherited method
+        print(f"Savings Balance: {self.savings_amount}")
