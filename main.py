d51acad11b4b5fcedb45dae4cacf45bb8b57481a
Bita Ghaffari <bghaffar@charlotte.edu>
2025-02-15 13:32:22 -0500

    Updated main.py with CheckingAccount test cases

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 
 # class named BankAccount
+from CheckingAccount import CheckingAccount
 class BankAccount:
     # class attribute
     bank_title = "Bank of the Cross"
@@ -56,4 +57,34 @@ bank_account2.print_customer_information()
 bank_account2.deposit(1000)
 bank_account2.withdraw(500)
 bank_account2.withdraw(10000) # should invoke the error message
+
+
+# Creating instances of CheckingAccount
+checking1 = CheckingAccount("Emily Johnson", 5000, 100, "987654321", "123456789", 2000)
+checking2 = CheckingAccount("Michael Smith", 3000, 50, "567890123", "987654321", 1500)
+
+# Running tests for CheckingAccount class
+print("\n--- Checking Account Operations ---")
+
+# Display account details
+checking1.print_account_information()
+checking2.print_account_information()
+
+# Performing transactions
+print("\nPerforming transactions...\n")
+
+# Emily withdraws $600
+checking1.withdraw(600)
+
+# Michael withdraws $4000 (exceeds balance)
+checking2.withdraw(4000)
+
+# Emily transfers $1500 to Michael
+checking1.transfer(1500, checking2)
+
+# Show final account details
+print("\n--- Final Account Details ---")
+checking1.print_account_information()
+checking2.print_account_information()
+
 
